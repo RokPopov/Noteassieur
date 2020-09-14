2bce92b2221f7b2042d2fe8a039aeb5c3958dde5
rokpopov <rokpopov90@gmail.com>
2020-09-14 17:36:58 +0200

    Add canvas component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Canvas from "./components/canvas";
 
 function App() {
   return (
     <div className="App">
       <h1>Something arbitrary</h1>
+      <Canvas />
     </div>
   );
 }
