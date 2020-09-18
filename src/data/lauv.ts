60845816d7c744553d9b4f6f4d96eb6cad5ad90e
Bartan89 <bart.kuijper89@gmail.com>
2020-09-18 10:56:52 +0200

    adding song perhaps for later

---
+++ src/data/lauv.ts
@@ -0,0 +1,27 @@
+export const blabla = "blabla"
+
+// const data = [
+//   { timecode: 0, lyrics: "[Music]" },
+//   { timecode: 9, lyrics: "New York City - no no no that all good" },
+//   { timecode: 16, lyrics: "yeah with me to be drunk in love in New" },
+//   { timecode: 20, lyrics: "York City in the morning coffee I mean" },
+//   { timecode: 40, lyrics: "from the sky I say far Lankans me like" },
+//   { timecode: 47, lyrics: "me" },
+//   { timecode: 54, lyrics: "[Music]" },
+//   { timecode: 60, lyrics: "[Music]" },
+//   { timecode: 69, lyrics: "I don't know what it is when I got that" },
+//   { timecode: 73, lyrics: "feeling waking up in this bed that's the" },
+//   { timecode: 77, lyrics: "hills where the room got no ceiling if" },
+//   { timecode: 81, lyrics: "we lay that long" },
+//   { timecode: "01:48", lyrics: "[Music]" },
+//   { timecode: "02:02", lyrics: "[Music]" },
+//   { timecode: "02:12", lyrics: "[Music]" },
+//   { timecode: "02:14", lyrics: "stay away stay with me stay away stay" },
+//   { timecode: "02:21", lyrics: "[Music]" },
+//   { timecode: "02:22", lyrics: "stay stay stay I knew from the first" },
+//   { timecode: "02:45", lyrics: "time" },
+//   { timecode: "02:49", lyrics: "[Music]" },
+//   { timecode: "02:55", lyrics: "[Music]" },
+//   { timecode: "03:06", lyrics: "[Music]" },
+//   { timecode: "03:14", lyrics: "oh yeah" }
+// ]
