796bd6fe408ed37fb52dcc2e393274254fdc558f
rokpopov <rokpopov90@gmail.com>
2020-09-18 10:30:21 +0200

    Create new component, configure page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,40 @@
-import React, { useEffect, useState } from "react"
-import "./App.css"
-import NoteTaker from "./components/canvas/NoteTaker"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import NoteTaker from "./components/canvas/NoteTaker";
 
-import VideoList from "./components/canvas/VideoPlayer/VideoList"
-import { useHistory, useLocation } from "react-router-dom"
-import { useDispatch, useSelector } from "react-redux"
-import { stageCurVideo } from "./store/video/actions"
+import VideoList from "./components/canvas/VideoPlayer/VideoList";
+import { useHistory, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { stageCurVideo } from "./store/video/actions";
+import Title from "./components/Title";
 
 function App() {
-  const history = useHistory()
+  const history = useHistory();
 
-  const location = useLocation()
+  const location = useLocation();
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const [num, setNum] = useState(5)
+  const [num, setNum] = useState(5);
 
   useEffect(() => {
     if (num > 0) {
-      setNum(num - 1)
-      const params = new URLSearchParams(location.search)
-      const VideoUrl = params.get("url")
-
-     
+      setNum(num - 1);
+      const params = new URLSearchParams(location.search);
+      const VideoUrl = params.get("url");
 
       if (VideoUrl) {
-        dispatch(stageCurVideo(VideoUrl))
+        dispatch(stageCurVideo(VideoUrl));
       }
     }
-  }, [])
+  }, []);
 
   return (
     <div className="App">
-      <h1>Noteassieur</h1>
+      <Title />
       <VideoList />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
